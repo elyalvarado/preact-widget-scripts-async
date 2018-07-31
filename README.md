3731403fdc8d2659aa3cc35f433ecae40b1943a5
Ely Alvarado <elyalvarado@gmail.com>
2018-07-31 14:46:09 -0500

    Modified babel config to use the async to promises babel plugin

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# Preact Widget Scripts
+# Preact Widget Scripts Async
 
 ##### Bunch of common scripts used in creating a preact widget.
+
+This package differs from preact-widget-scripts by changing the babel configuration
+to support async/await with `babel-plugin-async-to-promises`
--- configs/babelrc.js
+++ configs/babelrc.js
@@ -11,16 +11,20 @@ module.exports = {
         targets: {
           browsers: ["> 100%", "last 200 versions", "IE >= 19"]
         },
-        exclude: ["transform-regenerator", "transform-es2015-typeof-symbol"]
+        exclude: ["transform-regenerator", "transform-es2015-typeof-symbol", "transform-async-to-generator"]
       }
-    ],
-    require.resolve("babel-preset-stage-1")
+    ]
   ],
   plugins: [
     require.resolve("babel-plugin-transform-object-assign"),
     require.resolve("babel-plugin-transform-decorators-legacy"),
     require.resolve("babel-plugin-transform-react-constant-elements"),
     require.resolve("babel-plugin-transform-react-remove-prop-types"),
+    require.resolve('babel-plugin-transform-export-extensions'),
+    require.resolve('babel-plugin-transform-class-properties'),
+    require.resolve('babel-plugin-transform-object-rest-spread'),
+    require.resolve('babel-plugin-syntax-dynamic-import'),
+    require.resolve('babel-plugin-async-to-promises'),
     [require.resolve("babel-plugin-transform-react-jsx"), { pragma: "h" }],
     [
       require.resolve("babel-plugin-jsx-pragmatic"),
